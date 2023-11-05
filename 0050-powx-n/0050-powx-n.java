9d445f9ba5128995fe90c075e2a0488499180236
shamgirhe <131247199+shamgirhe@users.noreply.github.com>
2023-11-05 13:03:05 +0530

    Time: 0 ms (100%), Space: 41 MB (28.97%) - LeetHub

---
+++ 0050-powx-n/0050-powx-n.java
@@ -0,0 +1,10 @@
+class Solution {
+    public double myPow(double x, int n) {
+        // double a = Math.abs(n);
+        // double squr =Math.abs( Math.pow(x,a));
+        // System.out.println(squr);
+        // if(squr==0.0) return 0;
+        // if(n<0) return (1.0/squr);
+       return Math.pow(x,n);
+    }
+}
